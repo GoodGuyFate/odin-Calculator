880da41c53fca470e697b4903d4559023d15fac6
GoodGuyFate <xgamerfatex@gmail.com>
2026-01-07 16:40:52 +0200

    Work on frontend of calculator

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function add(x, y) {
+	return Number(x) + Number(y)
+}
+
+function subtract(x, y) {
+	return Number(x) - Number(y)
+}
+
+function multiply(x, y) {
+    return Number(x) * Number(y)
+}
+
+function divide(x, y) {
+    if (y === 0) return "Error"
+    return Number(x) / Number(y)
+}
+
